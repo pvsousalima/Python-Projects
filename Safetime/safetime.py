73f193ef69ce1854b9b0df6db407bc0850bee738
Pedro Victor <pvsousalima@hotmail.com>
2013-10-15 13:53:33 -0300

    Daemon implementado

--- Safetime/safetime.py
+++ Safetime/safetime.py
@@ -1,23 +1,41 @@
 #-*- coding: utf-8 -*-
 
 import os
-from datetime import date
+import time
+from daemon import runner
 
 
+# Create the daemon object
 def main():
-
-    # get the date actual date
-    today = date.today()
-
-    hj = date(today.year, 10, 14)
-
-    if today == hj:
-        notification()
+    notifier = Runner()
+    daemon_runner = runner.DaemonRunner(notifier)
+    daemon_runner.do_action()
 
 
+# Function to notify
 def notification():
-    os.system(
-        'notify-send \'Título da minha notificação\' \'Texto da minha pequena notificação\'')
+    os.system('notify-send \'1 hora de utilização\' \'Descansar.\'')
+
+
+# Function to shows the succeed to activate the notification
+def success():
+    os.system('notify-send \'Notificador ativado\'')
+
+
+class Runner():
+
+    def __init__(self):
+        self.stdin_path = '/dev/null'
+        self.stdout_path = '/dev/tty'
+        self.stderr_path = '/dev/tty'
+        self.pidfile_path = '/tmp/foo.pid'
+        self.pidfile_timeout = 5
+
+    def run(self):
+        success()
+        while(True):
+            time.sleep(3600)
+            notification()
 
 
 if __name__ == '__main__':
